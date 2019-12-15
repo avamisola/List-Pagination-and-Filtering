a405cd011b59dc4fd0b0353338181022b8663509
avamisola <58097632+avamisola@users.noreply.github.com>
2019-12-15 01:53:07 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,27 +7,24 @@ FSJS project 2 - List Filter and Pagination
 
 const studentList = document.getElementsByClassName("student-item");
 const pageLimit = 10;
-//console.log(studentList)
 
 //
 
 const showPage = (list, page) => {
    const startIndex = (page * pageLimit) - pageLimit;
    const endIndex = page * pageLimit;
-   for (let i = 0; i < studentList.length; i++) {
+   for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-         //console.log(studentList[i]);
-         console.log(i);
+         studentList[i].style.display = "block";
+      } else {
+         studentList[i].style.display = "none";
       }
    }
 };
 
 showPage(studentList.length, 6);
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+//
 
 const appendPageLinks = (list) => {
    const divPage = document.querySelector(".page");
@@ -38,19 +35,36 @@ const appendPageLinks = (list) => {
    divPage.appendChild(div);
    div.appendChild(ul);
    for (let i = 0; i < maxPages; i++) {
+      const page = i + 1
       const li = document.createElement("li");
       const a = document.createElement("a");
-      a.href = i + 1;
-      a.textContent = i + 1;
+      a.textContent = page;
       ul.appendChild(li);
       li.appendChild(a);
+   if (page == 1) {
+      a.className = "active";
    }
-   console.log(maxPages);
+   a.addEventListener("click", (e) => {
+      links = document.querySelectorAll("a.active");
+      console.log(links);
+      for (let i = 0; i < links.length; i++) {
+         links[i].className = "";
+      }
+      //const activeLink = document.getElementsByClassName("active");
+      //console.log(activeLink);
+      //activeLink.className = "";
+      e.target.className = "active";
+      showPage(list, page)
+
+      });
+   }
+
    console.log(ul);
 };
 
+//
 
-
+showPage(studentList, 1);
 appendPageLinks(studentList);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
